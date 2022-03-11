7f098df7ac6cd7b447718c53fb64e53496bbbc11
li02.wang <li02.wang@horizon.ai>
2022-03-11 10:44:30 +0800

    change README.md and install.sh

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vim-configuration
-vim configuration
+1.Execute install.sh
+2.Set the node environment variable
+3.Execute PlugInstall in vim command mode
--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 mkdir ~/.vim
 cp ./vimrc ~/.vim
 git clone https://github.com/junegunn/vim-plug
+cp vim-plug/plug.vim ~/.vim/autoload/
+xz -d ./node-v16.14.0-linux-x64.tar.xz
+tar xvf node-v16.14.0-linux-x64.tar
